b8947a471ba4a29dc65639bdef8bbc1cb69046ec
dim4 <dmitrvls@gmail.com>
2024-03-24 22:32:33 +0600

    Основная верстка главной вкладки готова

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         ZStack {
@@ -16,56 +17,83 @@ struct ContentView: View {
                     .font(.system(size: 16, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
-                VStack (alignment: .leading) {
-                    Text("Right now")
-                    HStack (spacing: 16) {
-                        Image(systemName: "cloud.drizzle.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
-                        VStack (alignment: .leading) {
-                            Text("-3º")
-                                .font(.system(size: 70, weight: .bold))
-                                .foregroundColor(.white)
-                            Text("Feels like -9º")
-                                .foregroundStyle(Color.gray)
+                ScrollView {
+                    VStack (alignment: .leading) {
+                        Text("Right now")
+                            .font(.system(size: 42, weight: .bold, design: .default))
+                            .foregroundColor(.white)
+                        HStack (spacing: 16) {
+                            Image(systemName: "cloud.drizzle.fill")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                            VStack (alignment: .leading) {
+                                Text("-3º")
+                                    .font(.system(size: 70, weight: .bold))
+                                    .foregroundColor(.white)
+                                Text("Feels like -9º")
+                                    .foregroundStyle(Color.gray)
+                            }
                         }
                     }
-                }
-                VStack {
-                    VStack (alignment: .leading) {
-                        Text("Tonight and tomorrow")
-                            .font(.title)
-                            .fontWeight(.bold)
-                        Text("Mostly clear tonight. Tomorrow will be a little warmer and partly cloudy.")
-                    }.padding(.all, 15)
-                    HStack (alignment: .bottom, spacing: 10) {
-                        WeatherHour(time: "21:00", temperature: 0)
-                        WeatherHour(time: "21:00", temperature: -15)
-                        WeatherHour(time: "21:00", temperature: -34)
-                        WeatherHour(time: "21:00", temperature: 34)
-                        WeatherHour(time: "21:00", temperature: 50)
-                        WeatherHour(time: "21:00", temperature: 99)
-                    }.padding(.top, 20)
-                }
-                VStack {
+                    VStack {
+                        VStack (alignment: .leading) {
+                            Text("Tonight and tomorrow")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            Text("Mostly clear tonight. Tomorrow will be a little warmer and partly cloudy.")
+                        }.padding(.all, 15)
+                        TabView {
+                            HStack (alignment: .bottom, spacing: 10) {
+                                WeatherHour(time: "21:00", temperature: 0)
+                                WeatherHour(time: "21:00", temperature: -15)
+                                WeatherHour(time: "21:00", temperature: -34)
+                                WeatherHour(time: "21:00", temperature: 34)
+                                WeatherHour(time: "21:00", temperature: 50)
+                                WeatherHour(time: "21:00", temperature: 99)
+                            }.padding(.top, 20)
+                            HStack (alignment: .bottom, spacing: 10) {
+                                WeatherHour(time: "00:00", temperature: 0)
+                                WeatherHour(time: "00:00", temperature: -15)
+                                WeatherHour(time: "00:00", temperature: -34)
+                                WeatherHour(time: "00:00", temperature: 34)
+                                WeatherHour(time: "00:00", temperature: 50)
+                                WeatherHour(time: "00:00", temperature: 99)
+                            }.padding(.top, 20)
+                        }
+                        .tabViewStyle(.page)
+                        .frame(height: 230)
+                        .onAppear {
+                            setIndicatorAppearance()
+                        }
+                    }
                     VStack (alignment: .leading) {
                         Text("This week")
                             .font(.title)
                             .fontWeight(.bold)
                         Text("Partly cloudy starting tomorrow, with highs near 0º for most of the week.")
                     }.padding(.all, 15)
-                    VStack {
-                        WeatherDay(weekDay: "TUE", date: 21, nightTemperature: -13, dayTemperatire: 1)
-                        WeatherDay(weekDay: "WED", date: 22, nightTemperature: -1, dayTemperatire: 14)
-                        WeatherDay(weekDay: "FRI", date: 23, nightTemperature: -99, dayTemperatire: 99)
-                        WeatherDay(weekDay: "SAT", date: 24, nightTemperature: 0, dayTemperatire: 1)
-                        
+                    TabView {
+                        VStack {
+                            WeatherDay(weekDay: "TUE", date: 21, nightTemperature: -13, dayTemperatire: 1)
+                            WeatherDay(weekDay: "WED", date: 22, nightTemperature: -1, dayTemperatire: 14)
+                            WeatherDay(weekDay: "THU", date: 24, nightTemperature: 19, dayTemperatire: 0)
+                            WeatherDay(weekDay: "FRI", date: 23, nightTemperature: -99, dayTemperatire: 99)
+                            WeatherDay(weekDay: "SAT", date: 24, nightTemperature: 0, dayTemperatire: 1)
+                            WeatherDay(weekDay: "SUN", date: 21, nightTemperature: -13, dayTemperatire: 1)
+                            WeatherDay(weekDay: "MON", date: 22, nightTemperature: -1, dayTemperatire: 14)
+                            WeatherDay(weekDay: "TUE", date: 23, nightTemperature: -99, dayTemperatire: 99)
+                        }
+                        Text("Datailed forecast is temporary unavailable")
                     }
+                    .tabViewStyle(.page)
+                    .frame(height: 550)
+                    .onAppear {
+                        setIndicatorAppearance()
+                    }
+                    Link(destination: URL(string: "https://google.com")!, label: {Text("About this data").underline()}).underline().padding(.top, 15)
                 }
-                Link(destination: URL(string: "https://google.com")!, label: {Text("About this data").underline()}).underline().padding(.top, 15)
-                Spacer()
             }
         }
     }
@@ -140,4 +168,9 @@ func getChartSize(temperature: Int, maxSize: Int) -> Int {
         default:
             return result
     }
+}
+
+func setIndicatorAppearance() {
+    UIPageControl.appearance().currentPageIndicatorTintColor = .gray
+    UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.2)
 }
